d6467d58b13f274a42af54c13a98daff1bca7016
yichigo <34562370+yichigo@users.noreply.github.com>
2018-08-16 04:40:35 -0500

    Add files via upload

---
+++ task.py
@@ -0,0 +1,71 @@
+import numpy as np
+from physics_sim import PhysicsSim
+
+class Task():
+    """Task (environment) that defines the goal and provides feedback to the agent."""
+    def __init__(self, init_pose=None, init_velocities=None, 
+        init_angle_velocities=None, runtime=5., target_pos=None):
+        """Initialize a Task object.
+        Params
+        ======
+            init_pose: initial position of the quadcopter in (x,y,z) dimensions and the Euler angles
+            init_velocities: initial velocity of the quadcopter in (x,y,z) dimensions
+            init_angle_velocities: initial radians/second for each of the three Euler angles
+            runtime: time limit for each episode
+            target_pos: target/goal (x,y,z) position for the agent
+        """
+        # Simulation
+        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
+        self.action_repeat = 3
+
+        self.state_size = self.action_repeat * 6
+        self.action_low = 0
+        self.action_high = 900
+        self.action_size = 4
+        
+        # last position
+        self.pose_last = self.sim.pose[:3]
+        
+        # Goal
+        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.]) 
+
+    def get_reward(self):
+        """Uses current pose of sim to return reward."""
+        #reward = 1.-0.3*abs(self.sim.pose[:3] - self.target_pos).sum()
+        
+        #distance = np.linalg.norm(self.target_pos - self.sim.pose[:3])
+        #distance_last = np.linalg.norm(self.target_pos - self.pose_last)
+        #reward = 0. + (distance_last - distance)
+        
+        #pose_diff = self.target_pos - self.sim.pose[:3]
+        #e_R = pose_diff/np.linalg.norm(pose_diff)
+        #e_v = self.sim.v/np.linalg.norm(self.sim.v)
+        #reward =  np.dot(self.sim.v, e_R)
+        
+        #reward = 1.-0.003*np.linalg.norm(self.target_pos - self.sim.pose[:3])
+        #reward = np.tanh(1.-.003*abs(self.sim.pose[:3] - self.target_pos) ).sum()
+        #reward = np.tanh(1.-.003*abs(self.sim.pose[:3] - self.target_pos).sum() )
+        reward_xy = np.tanh(1.-.01*abs(self.sim.pose[:2] - self.target_pos[:2])).sum()
+        reward_z = np.tanh(1.-.005*abs(self.sim.pose[2] - self.target_pos[2]))
+        reward = reward_xy + reward_z
+        
+        #reward = np.tanh(1.-.009*np.linalg.norm(self.sim.pose[:3] - self.target_pos) )
+        return reward
+
+    def step(self, rotor_speeds):
+        """Uses action to obtain next state, reward, done."""
+        reward = 0
+        pose_all = []
+        for _ in range(self.action_repeat):
+            self.pose_last = self.sim.pose[:3] # record last position
+            done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
+            reward += self.get_reward() 
+            pose_all.append(self.sim.pose)
+        next_state = np.concatenate(pose_all)
+        return next_state, reward, done
+
+    def reset(self):
+        """Reset the sim to start a new episode."""
+        self.sim.reset()
+        state = np.concatenate([self.sim.pose] * self.action_repeat) 
+        return state
